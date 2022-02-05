5f260e91551dd61e67e19c1bcb8c3af9de923b7b
Andrew Booker <improvizone@googlemail.com>
2022-02-05 15:03:49 +0000

    updating the qrcode to point to the youtube playlist instead of the website

--- qr.py
+++ qr.py
@@ -5,7 +5,7 @@ import sys
 
 www = sys.argv[1]
 
-qr = qrcode.QRCode(version=1, box_size=6, border=2)
+qr = qrcode.QRCode(version=1, box_size=4, border=2)
 qr.add_data(www)
 qr.make(fit=True)
 img = qr.make_image(fill="black", back_color="white")
