5704063e4aaafb5b06617233194aeaef23cc8d66
Andrew Booker <improvizone@gmail.com>
2024-06-02 12:02:33 +0100

    simplifying postcard size using ready-made sizing

--- utils/postcard.py
+++ utils/postcard.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-A6 = [111, 154]
-DPI = 300
-INCH_PER_MM = 1000 / 2.54
-
-SIZE = tuple([int(DPI * INCH_PER_MM / m) for m in A6])
-print(SIZE)
-
-
 from reportlab.pdfgen.canvas import Canvas
-canvas = Canvas("thing.pdf", pagesize=SIZE)
-canvas.drawString(72, 72, "Randomatones")
+from reportlab.lib.pagesizes import A6, landscape
+
+
+canvas = Canvas("thing.pdf", pagesize=landscape(A6))
+canvas.drawString(72, 144, "Randomatones")
 canvas.save()
