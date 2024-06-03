74674f72b561aafc77d24b40e8343ddb978ca61d
Andrew Booker <improvizone@gmail.com>
2024-06-03 22:58:56 +0100

    adding placeholder text for picture key

--- utils/postcard.py
+++ utils/postcard.py
@@ -7,7 +7,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 
 def add_rhs_address(canvas, size):
     canvas.setLineWidth(0.5)
-    canvas.setStrokeColor("grey")
+    canvas.setStrokeColor("lightgrey")
     half_x = size[0] / 2
     y_margin = 10
     canvas.line(half_x, y_margin, half_x, size[1] - y_margin)
@@ -35,8 +35,39 @@ def add_heading(canvas, size):
     canvas.drawText(t)
 
 
+def add_upper_details(canvas, size):
+    t = canvas.beginText()
+    margin = 10
+
+    t.setTextOrigin(margin, size[1] - 36)
+    t.setFont("Helvetica", 8)
+    t.textLine("Pop-up aleatoric music installations")
+
+    t.setFont("Helvetica", 5)
+    loc_key = [
+        "Walthamstow Forest",
+        "Woolwich Foot Tunnel",
+        "Cwmbran Tunnel"
+    ]
+    for i in range(len(loc_key)):
+        t.textLine(f"{i + 1}. {loc_key[i]}")
+
+    canvas.drawText(t)
+
+
+def add_lower_details(canvas, size):
+    pass
+
+
 size = landscape(A6)
 canvas = Canvas("postcard_reverse.pdf", pagesize=size)
-add_heading(canvas, size)
-add_rhs_address(canvas, size)
+items = {
+    add_heading,
+    add_upper_details,
+    add_lower_details,
+    add_rhs_address
+}
+
+for i in items:
+    i(canvas, size)
 canvas.save()
