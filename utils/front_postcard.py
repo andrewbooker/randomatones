9b375842e746fa6d64112daecb5ea6d157c5c106
Andrew Booker <improvizone@gmail.com>
2024-06-16 16:08:04 +0100

    consolidating image urls

--- utils/front_postcard.py
+++ utils/front_postcard.py
@@ -15,10 +15,10 @@ size = (tuple([t + (bleed * 2) for t in landscape(A6)]))
 canvas = Canvas("postcard_front.pdf", pagesize=size)
 
 w_imgs = [
-    ("https://live.staticflickr.com/65535/53794930691_7fe02bc0dc_b.jpg", 0.2, "Walthamstow Forest"),
-    ("https://live.staticflickr.com/65535/53795241949_051ecc1248_b.jpg", 0.9, "South Woodford"),
-    ("https://live.staticflickr.com/65535/53795150323_693d6768b7_b.jpg", 0.1, "Snarestone Tunnel"),
-    ("https://live.staticflickr.com/65535/53795241979_b0e61dd7f7_b.jpg", 1.0, "Woolwich Foot Tunnel"),
+    ("53794930691_7fe02bc0dc", 0.2, "Walthamstow Forest"),
+    ("53795241949_051ecc1248", 0.9, "South Woodford"),
+    ("53795150323_693d6768b7", 0.1, "Snarestone Tunnel"),
+    ("53795241979_b0e61dd7f7", 1.0, "Woolwich Foot Tunnel"),
 ]
 
 x_2 = size[0] / 2
@@ -26,7 +26,7 @@ y_2 = size[1] / 2
 ratio = size[0] / size[1]
 
 for i in range(4):
-    get_resp = requests.get(w_imgs[i][0])
+    get_resp = requests.get(f"https://live.staticflickr.com/65535/{w_imgs[i][0]}_b.jpg")
     if get_resp.status_code != 200:
         break
 
@@ -38,10 +38,8 @@ for i in range(4):
     bottom = pil_img.height
     left = surplus_width * w_imgs[i][1]
     right = left + required_width
-    cr = (left, top, right, bottom)
-    print(pil_img.size, required_width, cr)
 
-    img = ImageReader(pil_img.crop(cr))
+    img = ImageReader(pil_img.crop((left, top, right, bottom)))
     canvas.drawImage(img, (i % 2) * x_2, (int(0.5 * i) % 2) * y_2, width=x_2, height=y_2, mask="auto")
 
 canvas.save()
