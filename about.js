d65f5215647be4db686c277b1820daa9d60e087d
Andrew Booker <improvizone@gmail.com>
2024-06-30 16:06:52 +0100

    settling on reasonable about page

--- about.js
+++ about.js
@@ -1,12 +1,15 @@
 [
     {
-        "heading": "About Randomatones",
-        "image": "https://live.staticflickr.com/65535/53793997999_6d267c2368_b.jpg",
-        "text": "Randomatones are an ongoing sonic art exploration by software developer and musician Andrew Booker. The original idea was to create ambient music using a kind of distributed looping process that removed all regularity of rhythm and timing. The technique of applying a slow attack to sampled sounds and playing them at random points in time is entirely suited for creating ambient music. During initial development, the idea digressed from the playing of samples to having the units generate their own sounds. One principle that has remained during their evolution is that the devices should be visible, obviously identifiable as the sources of the sound, and somewhat interesting to look at. Another is that they are ideally used in places with good natural reverb."
-    },
-    {
         "heading": "About Andrew Booker",
         "image": "https://live.staticflickr.com/65535/53656446151_de8cb2f22d_b.jpg",
-        "text": "Andrew Booker is a professional software developer and has been an amateur musician since his teens, producing his first self-financed solo mini-album in the 1990s. As a prog drummer, over the past two decades he has recorded, or at least performed, with No-man, Tim Bowness, Sanguine Hum, Rain and Beatrix Players, appearing live in the UK, Netherlands, Germany, Poland, and USA. Also he has played on and produced recordings with Peter Banks (ex Yes) and David Cross (ex King Crimson). From 2007-2010 Andrew ran Improvizone, a semi-regular improvisation evening playing ambient electronic chillout music. Improvizone has occasionally resurfaced since then, and for an appearance in 2019 Andrew put together a software-based randomising voice sample player, automated so as not to require free hands to play it. He later developed this technology into the Randomatones, his main artistic activity since 2020. Most recently, when not paddling into canal tunnels to shoot Randomatones videos, Andrew can be found playing large Brazilian and Italian tambourines and singing in a (mainly) covers trio, gigging within easy reach of North East London."
+        "text": "Andrew Booker is a software developer and busy amateur musician. Over the last two decades he has appeared with prog/art rock acts such as No-man, Tim Bowness, Sanguine Hum, Rain and Beatrix Players, as a drummer. At the same time he has nurtured a parallel interest in ambient music. From 2007-2010 he ran <a target=\"_blank\" href=\"http://improvizone.org/\">Improvizone</a>, a semi-regular London-based evening of ambient electronic chillout music. Improvizone has occasionally resurfaced since then, and for an appearance in 2019 he put together a software-based randomising sample player that required no free hands to play it. He later developed this idea into the Randomatones, which have been his main artistic activity since 2020."
+    },
+    {
+        "heading": "About Randomatones",
+        "image": "https://live.staticflickr.com/65535/53793997999_6d267c2368_b.jpg",
+        "text": "Randomatones are an ongoing exploration of ambient music as sonic art by Andrew Booker. The first Randomatones setup comprised a central sampler, with satellite devices collecting samples from it and playing them at random. In a subsequent simpler and more portable setup, the devices created their own sounds, giving rise to the idea of presenting these devices in locations with generous acoustics. Andrew has built up a <a href=\"./portfolio.html\">portfolio of location videos</a> of the devices in settings with large natural reverb, his favourites being canal tunnels of the South Midlands in wintertime. As objects for video, Andrew likes the units to feature plenting of their own lighting, especially in the more recent versions. In May 2024 the Randomatones made their first live appearance in front of an audience, providing pre-show/interval/post-show sounds for a music and spoken word <a href=\"./index.html#2024-05-27\">performance in Highams Park</a>, North East London."
     }
 ]
+
+
+
