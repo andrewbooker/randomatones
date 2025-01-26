d44409cc848f229bf50a4a4dfba6a63ed325a773
Andrew Booker <improvizone@gmail.com>
2025-01-26 20:33:39 +0000

    updating feature to one-week-to-go talk

--- build.py
+++ build.py
@@ -204,7 +204,7 @@ class PortfolioPage(TemplateDoc):
             ("k2WSEi149CM", "53794930691_7fe02bc0dc"),
             ("-Iec5qIazRc", "53795241949_051ecc1248")
         ]
-        self.feature = "Ug5JnOnAPmA"
+        self.feature = "iNJCQX3eocQ"
         self.omit_items = {
             "eIS7tWrvk-c",   #pumpkin
             "6W2pAlF3pl8",   #armitage,
