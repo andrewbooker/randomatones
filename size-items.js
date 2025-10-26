4d11ffabb14b7d5ea1e45361510e6ff92aadd3d3
Andrew Booker <improvizone@gmail.com>
2025-10-26 13:28:09 +0000

    consolidating sizing into single class

--- size-items.js
+++ size-items.js
@@ -1,25 +1,4 @@
-function sizeFonts() {
-    const pxRatio = (function() {
-        if (window.devicePixelRatio === 1.0) {
-            return 1.0;
-        }
-        const isHorizontal = (window.innerWidth / window.innerHeight) > 1.0;
-        return (isHorizontal ? 0.5 : 1.0) * Math.max(1.0, window.devicePixelRatio * 0.7);
-    })();
-
-    const ratios = {
-        "when": 150,
-        "post-heading": 150,
-        "post-text": 100,
-        "previous-year": 100,
-        "current-year": 100
-    };
-    Object.entries(ratios).forEach(([c, r]) => {
-        Array.from(document.getElementsByClassName(c)).forEach(t => {
-            t.setAttribute("style", "font-size: " + (r * pxRatio) + "%");
-        });
-    });
-}
+function sizeFonts() {}
 
 class Resize {
     constructor(leftMargin, lmOnlyItems, lmOffset) {
@@ -55,6 +34,30 @@ class Resize {
         return this;
     }
 
+    fonts() {
+        const pxRatio = (function() {
+            if (window.devicePixelRatio === 1.0) {
+                return 1.0;
+            }
+            const isHorizontal = (window.innerWidth / window.innerHeight) > 1.0;
+            return (isHorizontal ? 0.5 : 1.0) * Math.max(1.0, window.devicePixelRatio * 0.7);
+        })();
+
+        const ratios = {
+            "when": 150,
+            "post-heading": 150,
+            "post-text": 100,
+            "previous-year": 100,
+            "current-year": 100
+        };
+        Object.entries(ratios).forEach(([c, r]) => {
+            Array.from(document.getElementsByClassName(c)).forEach(t => {
+                this.setStyle(t, "font-size: " + (r * pxRatio) + "%");
+            });
+        });
+        return this;
+    }
+
     render() {
         for (let [e, ss] of this.styles) {
             e.setAttribute("style", Array.from(ss).join("; "));
@@ -63,5 +66,5 @@ class Resize {
 }
 
 function sizeMargins(leftMargin, lmOnlyItems, lmOffset) {
-    new Resize(leftMargin, lmOnlyItems, lmOffset).margins().render();
+    new Resize(leftMargin, lmOnlyItems, lmOffset).margins().fonts().render();
 }
