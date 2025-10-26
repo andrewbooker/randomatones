b389d7847532178b500d9b73905a190a4cc2eb13
Andrew Booker <improvizone@gmail.com>
2025-10-26 11:55:58 +0000

    merging item resizing into a single js file

--- size-items.js
+++ size-items.js
@@ -20,3 +20,20 @@ function sizeFonts() {
         });
     });
 }
+
+function sizeMargins(leftMargin, lmOnlyItems, lmOffset) {
+    let lm = leftMargin;
+    let rm = window.innerWidth / 24.0;
+    const m = (window.innerWidth / 2) - 640;
+    if (m > 0) {
+        lm += m;
+        rm += m;
+    }
+    document.getElementById("links").setAttribute("style", "margin-right:" + rm + "px; margin-left:" + lm + "px;");
+    Object.entries(lmOnlyItems).forEach(([id, get]) => {
+        const e = get(id);
+        if (e) {
+            e.setAttribute("style", "margin-left:" + (lm + lmOffset) + "px;");
+        }
+    });
+}
