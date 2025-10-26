82ab4a2470ff404d72846d5740c453a410db6b81
Andrew Booker <improvizone@gmail.com>
2025-10-26 08:34:50 +0000

    consolidating right margin setting

--- resize.js
+++ resize.js
@@ -1,12 +1,6 @@
 function resize() {
-    let lm =  Math.max(10, (window.innerWidth * 0.3) - (302 + (18 * 2)));
-    let rm = window.innerWidth / 24.0
-    const m = (window.innerWidth / 2) - 640;
-    if (m > 0) {
-        rm += m;
-    }
-
-    sizeMargins(lm, rm, {
+    const lm =  Math.max(10, (window.innerWidth * 0.3) - (302 + (18 * 2)));
+    sizeMargins(lm, {
         "content-by-year": (i) => document.getElementById(i),
         "h1": (t) => document.getElementsByTagName(t)[0]
     }, 20);
--- size-margins.js
+++ size-margins.js
@@ -1,6 +1,11 @@
-
-
-function sizeMargins(lm, rm, lmOnlyItems, lmOffset) {
+function sizeMargins(leftMargin, lmOnlyItems, lmOffset) {
+    let lm = leftMargin;
+    let rm = window.innerWidth / 24.0;
+    const m = (window.innerWidth / 2) - 640;
+    if (m > 0) {
+        lm += m;
+        rm += m;
+    }
     document.getElementById("links").setAttribute("style", "margin-right:" + rm + "px; margin-left:" + lm + "px;");
     Object.entries(lmOnlyItems).forEach(([id, get]) => {
         get(id).setAttribute("style", "margin-left:" + (lm + lmOffset) + "px;");
