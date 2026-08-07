b415db08a6f4a1ded98497cee8839d341ea71bd1
Andrew Booker <improvizone@gmail.com>
2026-08-07 21:06:51 +0100

    bottle entry including fix for date vs time lookup on image name

--- utils/grabOgImageUrlFrom.sh
+++ utils/grabOgImageUrlFrom.sh
@@ -9,7 +9,7 @@ grep 'meta property="og:title"' image.html >> image.xml
 echo '</meta>' >> image.xml
 echo '</details>' >> image.xml
 imgUrl=$(xmlstarlet sel -t -v '//details/meta[@property="og:image"]/@content' image.xml)
-imgDate=$(echo $(xmlstarlet sel -t -v '//details/meta[@property="og:title"]/@content' image.xml) | sed -E 's/.*(20[0-9]{6}).*/\1/' | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})/\1-\2-\3/')
+imgDate=$(echo $(xmlstarlet sel -t -v '//details/meta[@property="og:title"]/@content' image.xml) | sed -E 's/.*_(20[0-9]{6})_.*/\1/' | sed -E 's/([0-9]{4})([0-9]{2})([0-9]{2})/\1-\2-\3/')
 
 echo "{\"when\":\"${imgDate}\",\"heading\":\"\",\"image\":\"$imgUrl\",\"text\":\"\"}" | jq .
 
